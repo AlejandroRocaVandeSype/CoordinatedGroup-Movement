5f4cb9c5975f9fb790b8c4cda00ec9b9e4826c76
Alejandro Roca Vande Sype <roca-vandesype@hotmail.com>
2023-08-12 20:27:30 +0200

    Sprites for Unit selection and Slot
    For Visual Debugging purposes

--- Project/Assets/Scripts/Player.cs
+++ Project/Assets/Scripts/Player.cs
@@ -11,20 +11,37 @@ public class Player : MonoBehaviour
     const int MOUSE_LEFT_CLICK = 0;
     const int MOUSE_RIGHT_CLICK = 1;
 
+
+    const string GROUND_LAYER = "Ground";
+    const string OBSTACLE_LAYER = "Obstacle";
+
+
     public void Update()
     {
-        if(Input.GetMouseButtonUp(MOUSE_LEFT_CLICK))
+       CheckMouseInput();
+    }
+
+    private void CheckMouseInput()
+    {
+        if (Input.GetMouseButtonUp(MOUSE_LEFT_CLICK))
         {
             // Single click - Save the position where it was clicked
             // Use raycast to get the position where it hits on the ground
-            RaycastHit hit;
-            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+
+            // Ray from the Camera to mousePosition
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(ray, out hitInfo, LayerMask.GetMask(GROUND_LAYER)))
             {
-                _clickPosition = hit.point;
-            }     
+                // Only if the hit collider is not an Obstacle then we save the clicked position
+                if (hitInfo.collider.gameObject.layer != LayerMask.NameToLayer(OBSTACLE_LAYER))
+                {
+                    _clickPosition = hitInfo.point;
+                }
+            }
         }
     }
-
 
     public Vector3 ClickPosition
     {
