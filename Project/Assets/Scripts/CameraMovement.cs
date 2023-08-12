bc678965bd92fba9bf09e668375c7e8d04c4271d
Alejandro Roca Vande Sype <roca-vandesype@hotmail.com>
2023-08-12 11:38:47 +0200

    GitIgnore modified for Unity projects

---
+++ Project/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    private float _cameraSpeed = 35.0f;
+
+    private void FixedUpdate()
+    {
+        float verticalMovement = Input.GetAxis("VerticalMov");
+        float horizontalMovement = Input.GetAxis("HorizontalMov");
+
+        Vector3 movement = horizontalMovement * Vector3.right + verticalMovement * Vector3.forward;
+        movement = movement.normalized;
+        movement *= _cameraSpeed * Time.deltaTime;
+
+
+        transform.position += movement;
+   
+    }
+}
